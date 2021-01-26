89e2c01ffae04901774574c2eb23b22b8d0071fc
WimmermM <39947721+WimmermM@users.noreply.github.com>
2021-01-26 20:19:35 +0100

    Add files via upload

---
+++ java-test-default/src/main/java/com/etnetera/hr/data/JavaScriptFramework.java
@@ -0,0 +1,75 @@
+package com.etnetera.hr.data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Simple data entity describing basic properties of every JavaScript framework.
+ * 
+ * @author Etnetera
+ *
+ */
+@Entity
+public class JavaScriptFramework {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false, length = 30)
+	private String name;
+
+	@Column(nullable = false, length = 30)
+	private String version;
+
+	@Column(nullable = false, length = 30)
+	private LocalDate deprecationDate;
+
+
+	@Column(nullable = false, length = 30)
+	private int hypeLevel;
+
+	public JavaScriptFramework() {
+	}
+
+	public JavaScriptFramework(String name) {
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+
+	@Override
+	public String toString() {
+		return "JavaScriptFramework [id=" + id + ", name=" + name + "]";
+	}
+
+}
---
+++ java-test-default/src/main/java/com/etnetera/hr/service/JavaScriptFrameworkService.java
@@ -0,0 +1,16 @@
+package com.etnetera.hr.service;
+
+import com.etnetera.hr.data.JavaScriptFramework;
+
+public class JavaScriptFrameworkService {
+
+
+    public JavaScriptFramework addFramework(String name){
+        return new JavaScriptFramework(name);
+    }
+
+
+
+
+
+}
