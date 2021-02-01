aeb533e937871d83a12b678f99ba9f02f4b5827c
WimmermM <marek.wimmer@gmail.com>
2021-02-01 22:30:05 +0100

    more refactoring

--- java-test-default/src/main/java/com/etnetera/hr/repository/FrameworkVersionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.etnetera.hr.repository;
-
-import com.etnetera.hr.data.FrameworkVersion;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FrameworkVersionRepository extends CrudRepository<FrameworkVersion, Long> {
-}
